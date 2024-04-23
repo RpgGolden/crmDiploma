639cf3b04551e6152c934566482342ac46c5046d
Artur <artur.tonoyan.04@list.ru>
2024-04-23 22:44:57 +0300

    filter 1

--- web/src/components/TableRegistrar/TableRegistrar.jsx
+++ web/src/components/TableRegistrar/TableRegistrar.jsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import styles from "./TableRegistrar.module.scss";
 import { tableData, tableHead } from "./Data";
 
-function TableRegistrar(props) {
+function TableRegistrar() {
+  const tableDataMemo = useMemo(() => tableData, []);
+  const [filterShow, setFilterShow] = useState(false);
+  const [filtredData, setFiltredData] = useState(tableDataMemo);
+
+  const sherchData = (el) => {
+    const query = el.target.value;
+
+    setFiltredData(
+      tableDataMemo.filter((item) => {
+        return (
+          item.name.toLowerCase().includes(query.toLowerCase()) ||
+          item.surname.toLowerCase().includes(query.toLowerCase()) ||
+          item.patronymic.toLowerCase().includes(query.toLowerCase()) ||
+          item.snils.toLowerCase().includes(query.toLowerCase()) ||
+          item.passport.toLowerCase().includes(query.toLowerCase())
+        );
+      })
+    );
+  };
+
   return (
     <div className={styles.TableRegistrar}>
       <div className={styles.head}>
-        <button>
-          <img src="./img/filter.png" alt="filter" />
-          Фильтр
-        </button>
-        <label className={styles.labelradio} htmlFor="myRadio">
-          <input type="radio" id="men" name="gender" checked /> Мужской
-          <input type="radio" id="women" name="gender" /> Женский
-        </label>
-        <div className={styles.date}>
-          <input type="date" />
-          <div className={styles.lin}></div>
-          <input type="date" />
+        <div className={styles.filters}>
+          <button onClick={() => setFilterShow(!filterShow)}>
+            <img src="./img/filter.png" alt="filter" />
+            Фильтр
+          </button>
+          {filterShow && (
+            <>
+              <label className={styles.labelradio} htmlFor="myRadio">
+                <input type="checkbox" id="men" name="gender" /> Мужской
+                <input type="checkbox" id="women" name="gender" /> Женский
+              </label>
+              <div className={styles.date}>
+                <input type="date" />
+                <div className={styles.lin}></div>
+                <input type="date" />
+              </div>
+            </>
+          )}
         </div>
-        <div className={styles.search}>
-          <img src="./img/Search_light.png" alt="Search_light" />
-          <input placeholder="Поиск по ФИО, СНИЛС или паспорту" type="text" />
+        <div className={styles.right_block}>
+          <div className={styles.search}>
+            <img src="./img/Search_light.png" alt="Search_light" />
+            <input
+              onChange={sherchData}
+              placeholder="Поиск по ФИО, СНИЛС или паспорту"
+              type="text"
+            />
+          </div>
+          <button>Найти пациента</button>
         </div>
-        <button>Найти пациента</button>
       </div>
 
       <div className={styles.table}>
@@ -36,7 +68,7 @@ function TableRegistrar(props) {
             </tr>
           </thead>
           <tbody>
-            {tableData.map((row, index) => (
+            {filtredData.map((row) => (
               <tr key={row.id}>
                 {tableHead.map((el) => (
                   <td key={el.key}>{row[`${el.key}`]}</td>
@@ -45,6 +77,9 @@ function TableRegistrar(props) {
             ))}
           </tbody>
         </table>
+      </div>
+      <div className={styles.text_length}>
+        <p>Всего строк: {filtredData.length}</p>
       </div>
     </div>
   );
