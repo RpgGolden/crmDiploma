860d85ad50ca04502e0d142b675c5f3fd0faec0e
Никита <100412266+Niks6455@users.noreply.github.com>
2024-04-25 18:42:46 +0300

    2

--- web/src/components/MakeAppointment/MakeAppointment.jsx
+++ web/src/components/MakeAppointment/MakeAppointment.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import styles from "./MakeAppointment.module.scss";
+import HeadMenu from "../HeadMenu/HeadMenu";
 
 function MakeAppointment() {
   return (
     <div className={styles.MakeAppointment}>
-        <h1>запись на прием </h1>
+       <HeadMenu state={"ViewMyAppointment"}/>
+       <div className={styles.box}>
+            <div className={styles.container}>
+            <h1>запись на прием </h1>
+            <div>
+              <h3>Данные для записи</h3>
+              <p>ФИО пациента</p>
+              <input type="text" placeholder="Пароль"></input>
+              <input type="text" placeholder="Пароль"></input>
+              <input type="text" placeholder="Пароль"></input>
+              <input type="text" placeholder="Пароль"></input>
+
+            </div>
+            </div>
+        </div>
+      
     </div>
   );
 }
